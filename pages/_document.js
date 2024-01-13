d5ab70dbe0a569efac17b5d1d7bc58e248f683ec
Shahar Elwaya <shahar.al22@gmail.com>
2024-01-13 22:26:30 +0200

    MUI and Login page

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import {
+  DocumentHeadTags,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v13-pagesRouter';
+// or `v1X-pagesRouter` if you are using Next.js v1X
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html dir="rtl" lang="he">
+        <Head>
+          <DocumentHeadTags {...this.props} />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700&display=swap"></link>
+          {/* ... Other head tags */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
+
+export default MyDocument;
