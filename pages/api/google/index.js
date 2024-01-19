b5f47d56eb963ba95a3981a0ea00ce8aa66b8eee
may-avivi <mayavivi1412@gmail.com>
2024-01-19 16:42:18 +0200

    roni-test

--- pages/api/google/index.js
+++ pages/api/google/index.js
@@ -3,4 +3,5 @@ import { createMeeting } from "@/DB/meeting";
 export default async function handler(req, res) {
     const test = await createMeeting(req.body);
     res.status(200).json(test)
+    console.log("I am roni")
 }
