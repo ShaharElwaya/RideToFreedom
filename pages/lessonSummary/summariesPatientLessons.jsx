a81b0c66e3d04edce9faa73b236ea6051bef13fb
Shahar Elwaya <shahar.al22@gmail.com>
2024-02-16 19:03:24 +0200

    select filters

--- pages/lessonSummary/summariesPatientLessons.jsx
+++ pages/lessonSummary/summariesPatientLessons.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from "@mui/material";
+import {
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  Container,
+  Card,
+  CardContent,
+  Chip,
+} from "@mui/material";
 import PicAndHeadlines from "@/components/UI/picAndheadline";
 import PatientRow from "@/components/UI/patientRow";
 import style from "../../styles/summariesPatientLessons.module.css";
@@ -14,12 +26,20 @@ export default function SummariesPatientLessons() {
   const router = useRouter();
   const [lessons, setLessons] = useState([]);
   const [addTime, setAddTime] = useState("");
-  const [name, setName] = useState();
+  const [name, setName] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const { patientId } = router.query;
   const { type, id } = userStore.getState();
   const isSmallScreen = useMediaQuery("(max-width: 600px)");
   const [maxLettersGuideName, setMaxLettersGuideName] = useState(0);
+
+  // Filter state
+  const [filters, setFilters] = useState({
+    selectedGuides: [],
+    selectedLessonTypes: [],
+  });
+  const [guides, setGuides] = useState([]);
+  const [lessonTypes, setLessonTypes] = useState([]);
 
   const handleAdd = () => {
     const currentDate = new Date().toLocaleDateString("en-US", {
@@ -67,6 +87,17 @@ export default function SummariesPatientLessons() {
         setLessons(lessonsData.data);
         setName(patientNameData.name);
 
+        // Extract unique guide names and lesson types from the lessons array
+        const uniqueGuides = [
+          ...new Set(lessonsData.data.map((lesson) => lesson.guide_name)),
+        ];
+        const uniqueLessonTypes = [
+          ...new Set(lessonsData.data.map((lesson) => lesson.lesson_type)),
+        ];
+
+        // Set the extracted values to the state
+        setGuides(uniqueGuides);
+        setLessonTypes(uniqueLessonTypes);
         if (lessonsData.data.length > 0) {
           let maxLetters = 0;
           for (let i = 0; i < lessonsData.data.length; i++) {
@@ -99,29 +130,29 @@ export default function SummariesPatientLessons() {
       }
     }
 
-    async function checkPremission() {
+    async function checkPermission() {
       try {
         if (type === 1) {
           // Fetch comments for the specific lessonId
           const response = await axios.get(`/api/login/childrens?id=${id}`);
           let isOk = false;
-          
-          for(let i = 0; i < response.data.length && !isOk; i++) {
-            if(response.data[i].id == patientId){
+
+          for (let i = 0; i < response.data.length && !isOk; i++) {
+            if (response.data[i].id == patientId) {
               isOk = true;
             }
           }
 
-          if (isOk == false) {
+          if (!isOk) {
             router.back(); // Use await to wait for the navigation to complete
           }
         }
       } catch (error) {
         console.error("Error checking permission:", error);
       }
-    }    
+    }
 
-    checkPremission();
+    checkPermission();
     fetchData();
   }, []); // Empty dependency array to run the effect only once on mount
 
@@ -148,6 +179,30 @@ export default function SummariesPatientLessons() {
     }
   };
 
+  const handleFilterChange = (filterName, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterName]: value,
+    }));
+  };
+
+  const filteredLessons = lessons.filter((lesson) => {
+    const isGuideMatch =
+      filters.selectedGuides.length === 0 ||
+      filters.selectedGuides.includes(lesson.guide_name);
+
+    const isLessonTypeMatch =
+      filters.selectedLessonTypes.length === 0 ||
+      filters.selectedLessonTypes.includes(lesson.lesson_type);
+
+    return isGuideMatch && isLessonTypeMatch;
+  });
+
+  const selectStyle = {
+    width: "160px",
+    textAlign: "right",
+  };
+
   return (
     <>
       {isLoading && <LoadingSpinner />}
@@ -161,39 +216,89 @@ export default function SummariesPatientLessons() {
         primaryHeadline="סיכומי שיעורים"
         secondaryHeadline={name ? name : "No Name Data"}
       />
-      {type !== 1 && (
-        <div className={style.addButtonStyle}>
-          <Button onClick={handleAdd}>+ הוספת סיכום</Button>
-        </div>
-      )}
-      {lessons.map(
-        (lesson) =>
-          (type === 3 ||
-            type === 2 ||
-            (type === 1 && lesson.parent_permission)) && (
-            <div
-              key={lesson.lesson_id}
-              className={style.rowWrapper}
-              onClick={() => handleRowClick(lesson.lesson_id)}
+
+        {/* Filter Button */}
+        <div className={style.rightStyle}>
+          <h4>סננים:</h4>
+          {/* Filter Controls */}
+          <FormControl className={style.filterInput}>
+            <InputLabel> מדריך</InputLabel>
+            <Select
+              style={selectStyle}
+              label="מדריך"
+              multiple
+              value={filters.selectedGuides}
+              onChange={(e) =>
+                handleFilterChange("selectedGuides", e.target.value)
+              }
+              variant="outlined"
             >
-              <PatientRow
-                pictureName={lesson.type}
-                picturePath={`../${
-                  lesson.patient_gender === "F" ? "girlPic" : "boyPic"
-                }.png`}
-                date={formatDate(lesson.formatted_date)}
-                time={lesson.formatted_time}
-                name={lesson.guide_name}
-                maxTextLengthName={isSmallScreen ? 7 : maxLettersGuideName}
-                nameWidth={isSmallScreen ? 77 : maxLettersGuideName * 9}
-                lesson={lesson.lesson_type}
-                {...(isSmallScreen && {
-                  maxTextLengthLesson: 7,
-                })}
-              />
-            </div>
-          )
-      )}
+              {/* Replace with the actual list of guide names */}
+              {guides.map((guide) => (
+                <MenuItem key={guide} value={guide}>
+                  {guide}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl className={style.filterInput}>
+            <InputLabel>סוג שיעור</InputLabel>
+            <Select
+              style={selectStyle}
+              label="סוג שיעור"
+              multiple
+              value={filters.selectedLessonTypes}
+              onChange={(e) =>
+                handleFilterChange("selectedLessonTypes", e.target.value)
+              }
+              variant="outlined"
+            >
+              {/* Replace with the actual list of lesson types */}
+              {lessonTypes.map((lessonType) => (
+                <MenuItem key={lessonType} value={lessonType}>
+                  {lessonType}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        
+        {type !== 1 && (
+          <div className={style.addButtonStyle}>
+            <Button onClick={handleAdd}>+ הוספת סיכום</Button>
+          </div>
+        )}
+
+      {/* Display filtered lessons */}
+      {!isLoading &&
+        filteredLessons.map(
+          (lesson) =>
+            (type === 3 ||
+              type === 2 ||
+              (type === 1 && lesson.parent_permission)) && (
+              <div
+                key={lesson.lesson_id}
+                className={style.rowWrapper}
+                onClick={() => handleRowClick(lesson.lesson_id)}
+              >
+                <PatientRow
+                  pictureName={lesson.type}
+                  picturePath={`../${
+                    lesson.patient_gender === "F" ? "girlPic" : "boyPic"
+                  }.png`}
+                  date={formatDate(lesson.formatted_date)}
+                  time={lesson.formatted_time}
+                  name={lesson.guide_name}
+                  maxTextLengthName={isSmallScreen ? 7 : maxLettersGuideName}
+                  nameWidth={isSmallScreen ? 77 : maxLettersGuideName * 9}
+                  lesson={lesson.lesson_type}
+                  {...(isSmallScreen && {
+                    maxTextLengthLesson: 7,
+                  })}
+                />
+              </div>
+            )
+        )}
     </>
   );
 }
