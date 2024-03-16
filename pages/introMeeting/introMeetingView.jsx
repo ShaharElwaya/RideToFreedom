63b380c30e9f9ef3d048dab8f8828f36a7f8b167
Shahar Elwaya <shahar.al22@gmail.com>
2024-03-16 12:14:50 +0200

    permissions comment

--- pages/introMeeting/introMeetingView.jsx
+++ pages/introMeeting/introMeetingView.jsx
@@ -311,7 +311,7 @@ export default function ViewForm() {
         {showComments && ( // Render comments section only if there are comments
           <div className={style.container}>
             <h4>תגובות:</h4>
-            <h6>תגובות אישיות בעלות סימן עריכה, ניתן לערוך בלחיצה עליהן</h6>
+            {type !== 1 && <h6>תגובות אישיות בעלות סימן עריכה, ניתן לערוך בלחיצה עליהן</h6>}
             {isLoading ? (
               <p>Loading comments...</p>
             ) : (
